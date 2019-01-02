bb58bab205d39d0990b29eb15413aa57cd1facef
Dmitriy Maksimovich <d.maksimovich@vivaldi.net>
2019-01-02 19:25:39 +0300

    fixed pagination, sections view

--- guides/urls.py
+++ guides/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^sort/(?P<filter>[\w]+)/$', views.SortedIndexView.as_view(), name='index_sorted'),
     url(r'^section/(?P<section>[\w]+)/$', views.SectionView.as_view(), name='section'),
+    url(r'^sections_browser/$', views.SectionBrowserView.as_view(), name='sections_browser'),
     url(r'^(?P<pk>[0-9]+)/$', views.GuideView.as_view(), name='guide'),
     url(r'^about_us/$', views.about_us_view, name='about_us'),
 ]
--- guides/views.py
+++ guides/views.py
@@ -8,7 +8,7 @@ class IndexView(generic.ListView):
     model = Guide
     template_name = 'guides/index.html'
     context_object_name = 'guides_list'
-    paginate_by = 20
+    paginate_by = 3
 
     def get_queryset(self):
         return Guide.objects.order_by("-votes")
@@ -19,48 +19,57 @@ class IndexView(generic.ListView):
         return context
 
 
-class SortedIndexView(generic.ListView):
-    template_name = 'guides/index.html'
-
+class SortedIndexView(IndexView):
     def get_queryset(self):
-        return
-
-    def get_context_data(self, **kwargs):
-        context = super(SortedIndexView, self).get_context_data(**kwargs)
-        context['sections'] = Section.objects.order_by('guides_in_section')[:10]
         guides_filter = self.kwargs.get('filter', None)
         if not guides_filter or guides_filter == 'top_all_time':
-            guides = Guide.objects.order_by("-votes")[:10]
+            guides = Guide.objects.order_by("-votes")
         elif guides_filter == "top_month":
             current_month = datetime.date.today().month
             current_year = datetime.date.today().year
             guides = Guide.objects.filter(pub_date__gte=datetime.date(current_year, current_month, 1))
-            guides = guides.order_by("-votes")[:10]
+            guides = guides.order_by("-votes")
         elif guides_filter == 'new':
-            guides = Guide.objects.order_by("-pub_date")[:10]
+            guides = Guide.objects.order_by("-pub_date")
         else:
             guides = []
-        context['guides_list'] = guides
+        return guides
+
+    def get_context_data(self, **kwargs):
+        context = super(SortedIndexView, self).get_context_data(**kwargs)
+        context['sections'] = Section.objects.order_by('guides_in_section')[:10]
         return context
 
 
-class SectionView(generic.ListView):
-    template_name = 'guides/index.html'
-
+class SectionView(IndexView):
     def get_queryset(self):
-        return
+        section = self.kwargs.get('section', None)
+        return Guide.objects.filter(section=section).order_by("-votes")
 
     def get_context_data(self, **kwargs):
         context = super(SectionView, self).get_context_data(**kwargs)
         context['sections'] = Section.objects.order_by('guides_in_section')[:10]
-        section = self.kwargs.get('section', None)
-        context['guides_list'] = Guide.objects.filter(section=section).order_by("-votes")[:10]
+        return context
+
+
+class SectionBrowserView(generic.ListView):
+    paginate_by = None
+    template_name = 'guides/sections_browser.html'
+    context_object_name = 'sections_list'
+
+    def get_queryset(self):
+        return Section.objects.order_by("-guides_in_section")
+
+    def get_context_data(self, **kwargs):
+        context = super(SectionBrowserView, self).get_context_data(**kwargs)
+        context['sections'] = Section.objects.order_by('guides_in_section')[:10]
         return context
 
 
 class GuideView(generic.DetailView):
     model = Guide
     template_name = 'guides/guide.html'
+    context_object_name = 'guide'
 
 
 def about_us_view(request):
